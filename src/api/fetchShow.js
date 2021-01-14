998f00b28206fde7e836a8e14b0b97178f890f50
Brandon Maroni <brandon-maroni@lambdastudents.com>
2021-01-13 17:20:55 -0700

    trimmed axios call down

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -3,11 +3,7 @@ import axios from 'axios'
 const fetchShow = () => {
   axios
     .get("https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes")
-    .then(res => {
-      setShow(res.data);
-      setSeasons(formatSeasons(res.data._embedded.episodes));
-      }
-    )
-  }
+    .then(res => res.data);
+}
 
 export default fetchShow;
