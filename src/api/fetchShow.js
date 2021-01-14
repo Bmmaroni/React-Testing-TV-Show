1af74ef8abddfe313062bebe6742bb737b18d131
Brandon Maroni <brandon-maroni@lambdastudents.com>
2021-01-13 17:28:31 -0700

    added err to fetchShow

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 const fetchShow = () => {
   axios
     .get("https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes")
-    .then(res => res.data);
+    .then(res => res.data)
+    .catch(err => err)
 }
 
 export default fetchShow;
