8f86e56ac78194bfd52a0aca90df73d9c40be01d
Brandon Maroni <brandon-maroni@lambdastudents.com>
2021-01-13 18:32:03 -0700

    edits to App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -6,12 +6,10 @@ import { fetchShow as mockFetchShow} from './api/fetchShow';
 jest.mock('./api/fetchShow');
 
 const showData = {
-  data: [
-    {
-      name: 'Stranger Things',
-      summary: "A love letter to the '80s classics that captivated a generation, Stranger Things is set in 1983 Indiana, where a young boy vanishes into thin air. As friends, family and local police search for answers, they are drawn into an extraordinary mystery involving top-secret government experiments, terrifying supernatural forces and one very strange little girl."
-    }
-  ]
+  data: {
+    name: 'Stranger Things',
+    summary: "A love letter to the '80s classics that captivated a generation, Stranger Things is set in 1983 Indiana, where a young boy vanishes into thin air. As friends, family and local police search for answers, they are drawn into an extraordinary mystery involving top-secret government experiments, terrifying supernatural forces and one very strange little girl."
+  }
 }
 
 test('renders without errors', () => {
@@ -23,7 +21,7 @@ test('fetches and renders showData', async () => {
   const { rerender } = render(<App />);
   screen.debug();
   await act(async () => {
-    rerender(<App />);
+    await rerender(<App />);
     screen.debug();
   });
   const title = screen.getByRole('h1');
