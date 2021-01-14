e2ee55e4799088c1ba1b54024679df3d66766460
Brandon Maroni <brandon-maroni@lambdastudents.com>
2021-01-13 18:23:33 -0700

    editing tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import App from './App';
 
 import { fetchShow as mockFetchShow} from './api/fetchShow';
@@ -9,7 +9,7 @@ const showData = {
   data: [
     {
       name: 'Stranger Things',
-      language: 'English'
+      summary: "A love letter to the '80s classics that captivated a generation, Stranger Things is set in 1983 Indiana, where a young boy vanishes into thin air. As friends, family and local police search for answers, they are drawn into an extraordinary mystery involving top-secret government experiments, terrifying supernatural forces and one very strange little girl."
     }
   ]
 }
@@ -17,3 +17,18 @@ const showData = {
 test('renders without errors', () => {
   render(<App />)
 })
+
+test('fetches and renders showData', async () => {
+  mockFetchShow.mockResolvedValueOnce(showData);
+  const { rerender } = render(<App />);
+  screen.debug();
+  await act(async () => {
+    rerender(<App />);
+    screen.debug();
+  });
+  const title = screen.getByRole('h1');
+  
+  await waitFor(() => {
+    expect(title).toHaveTextContent(/stranger things/i);
+  });
+});
