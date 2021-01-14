8a24bc3d993049d921a4b28d97abc5e6f729e197
Brandon Maroni <brandon-maroni@lambdastudents.com>
2021-01-13 17:40:00 -0700

    edited fetchShow hook in App

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ export default function App() {
         setShow(res.data);
         setSeasons(formatSeasons(res.data._embedded.episodes));
       })
-    fetchShow();
   }, []);
 
   const handleSelect = e => {
