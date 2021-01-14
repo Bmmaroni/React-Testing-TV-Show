2598f47ef5288844fedca706adfbde25e48b0348
Brandon Maroni <brandon-maroni@lambdastudents.com>
2021-01-13 17:33:08 -0700

    edited fetchShow

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -1,10 +1,14 @@
 import axios from 'axios'
 
-const fetchShow = () => {
-  axios
+export const fetchShow = () => {
+  return axios
     .get("https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes")
-    .then(res => res.data)
-    .catch(err => err)
-}
-
-export default fetchShow;
+    .then(res => {
+      console.log(res);
+      return res;
+    })
+    .catch(err => {
+      console.log('Error fetching from the api', err.message);
+      return err;
+    });
+};
